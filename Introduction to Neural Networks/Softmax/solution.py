3df37ebc530d750f38182ebab7e8dd125bffe160
Akkireddy1 <challaakkireddy@gmail.com>
2018-12-12 13:55:06 +0100

    Introduction to Neural Networks
    Done

---
+++ Introduction to Neural Networks/Softmax/solution.py
@@ -0,0 +1,17 @@
+# This is the solution given by the course moderators.
+# It may differs that my own solution. It's been added here just to be reference.
+
+import numpy as np
+
+def softmax(L):
+    expL = np.exp(L)
+    sumExpL = sum(expL)
+    result = []
+    for i in expL:
+        result.append(i*1.0/sumExpL)
+    return result
+    
+    # Note: The function np.divide can also be used here, as follows:
+    # def softmax(L):
+    #     expL = np.exp(L)
+    #     return np.divide (expL, expL.sum())
